c7759e3419d104ddcc7934dcd7495e6d1abe1bc1
Igor Zhirkov <igorjirkov@gmail.com>
2023-05-14 13:01:01 +0200

    add ABI descriptions for calls and fat pointers

---
+++ src/ABI.v
@@ -0,0 +1,57 @@
+Require Memory.
+
+Import Common Memory.
+
+(** ABIs are described here:
+https://github.com/matter-labs/zkevm_opcode_defs/blob/v1.3.2/src/definitions/abi/far_call.rs
+ *)
+
+Record coder (ABIParams:Type) := {
+    decode: u256 -> ABIParams ;
+    encode:  ABIParams -> u256 ;
+    revertible1: forall params, decode (encode params) = params;
+    revertible2: forall params encoded, decode encoded = params -> encode params = encoded;
+  }.
+
+(** * Fat Pointers *)
+Module FatPointer.
+  Axiom ABI : coder fat_pointer.
+End FatPointer.
+
+
+(** * Ret *)
+Module Ret.
+  Inductive forward_page_type := UseHeap | ForwardFatPointer | UseAuxHeap.
+  Record params := {
+      memory_quasi_fat_pointer: fat_pointer;
+      page_forwarding_mode: forward_page_type;
+    }.
+
+  Axiom ABI: coder params.
+End Ret.
+
+(** * Near call *)
+Module NearCall.
+
+  Record params: Type := {
+      nca_get_ergs_passed: u32;
+    }.
+
+  Axiom ABI: coder params.
+
+End NearCall.
+
+(** * Far call *)
+Inductive far_call_forward_page_type := UseHeap | ForwardFatPointer | UseAuxHeap.
+
+Record far_call := {
+    fc_memory_quasi_fat_pointer: fat_pointer;
+    fc_ergs_passed: u32;
+    fc_shard_id: u8;
+    fc_forwarding_mode: far_call_forward_page_type;
+    fc_constructor_call: bool;
+    fc_consider_new_tx: bool;
+  }.
+
+
+Axiom ABI_far_call: coder far_call.
