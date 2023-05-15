2521c5a9a38b056363074ca8650b7ea8f1bf497e
Igor Zhirkov <igorjirkov@gmail.com>
2023-05-15 11:34:34 +0200

    add fat pointer manipulation and validation

--- src/ABI.v
+++ src/ABI.v
@@ -1,6 +1,6 @@
 Require Memory.
 
-Import Common Memory.
+Import Bool ZMod Common Memory.
 
 (** ABIs are described here:
 https://github.com/matter-labs/zkevm_opcode_defs/blob/v1.3.2/src/definitions/abi/far_call.rs
@@ -17,6 +17,39 @@ Record coder (ABIParams:Type) := {
 Module FatPointer.
   Axiom ABI : coder fat_ptr.
 
+  Record validation_exception :=
+    mk_ptr_validation_exception
+      {
+        ptr_expected_zero_offset : bool;
+        ptr_deref_beyond_heap_range : bool;
+      }.
+
+  Definition fat_ptr_empty :=
+    {|
+      fp_mem_page := 0;
+      fp_start := zero32;
+      fp_length:= zero32;
+      fp_offset:= zero32;
+    |}.
+
+  Definition is_overflowing {bits:nat} (res: int_mod bits* bool) := snd res.
+
+  Definition validate (p:fat_ptr) (fresh:bool) : validation_exception :=
+    {|
+      ptr_expected_zero_offset    := fresh && (p.(fp_offset) == zero32);
+      ptr_deref_beyond_heap_range := is_overflowing (p.(fp_start) + p.(fp_length))
+    |}.
+
+  (* FIXME the comment in Rust code mentions that the empty slice should be
+  accepted. Maybe we should rewrite it as: "length == 0 OR ofs < length" rather
+  than "offset <= length"? *)
+  Definition validate_as_slice (p:fat_ptr) : bool
+    := (le_unsigned _ p.(fp_offset) p.(fp_length) ).
+
+  Definition validate_in_bounds (p:fat_ptr) : bool := (lt_unsigned _ p.(fp_offset) p.(fp_length) ).
+
+  Definition is_trivial (p:fat_ptr) := (p.(fp_length) == zero32) && (p.(fp_offset)
+                                       == zero32).
 
 End FatPointer.
 
