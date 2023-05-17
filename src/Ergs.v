6420f66189b666b77dc5e8470f36cceff9ea7559
Igor Zhirkov <igorjirkov@gmail.com>
2023-05-17 12:38:18 +0200

    refactor ergs system

--- src/Ergs.v
+++ src/Ergs.v
@@ -1,47 +1,51 @@
-Require lib.ZMod Common.
+Require lib.ZMod Common Memory.
 
 (** * Ergs *)
 Section Ergs.
-Import Common ZMod ZArith.
+Import Common Memory ZMod ZArith.
 Open Scope Z_scope.
 
-Definition VM_CYCLE_COST_IN_ERGS: u32 := int_mod_of 32 4%Z.
-Definition RAM_PERMUTATION_COST_IN_ERGS: u32 := int_mod_of 32 1%Z.
-Definition CODE_DECOMMITMENT_COST_PER_WORD_IN_ERGS: u32 := int_mod_of 32 4%Z.
-Definition STORAGE_APPLICATION_COST_IN_ERGS: u32 := int_mod_of 32 678%Z.
-Definition CODE_DECOMMITTER_SORTER_COST_IN_ERGS: u32 := int_mod_of 32 1%Z.
-Definition LOG_DEMUXER_COST_IN_ERGS: u32 := int_mod_of 32 1%Z.
-Definition STORAGE_SORTER_COST_IN_ERGS: u32 := int_mod_of 32 2%Z.
-Definition EVENTS_OR_L1_MESSAGES_SORTER_COST_IN_ERGS: u32 := int_mod_of 32 1%Z.
-Definition INITIAL_WRITES_PUBDATA_HASHER_COST_IN_ERGS: u32 := int_mod_of 32 18%Z.
-Definition REPEATED_WRITES_PUBDATA_HASHER_COST_IN_ERGS: u32 := int_mod_of 32 11%Z.
-Definition CODE_DECOMMITMENT_SORTER_COST_IN_ERGS: u32 := int_mod_of 32 1%Z.
+Definition ergs := u32.
 
-Definition L1_MESSAGE_MIN_COST_IN_ERGS: u32 := int_mod_of 32 156250%Z.
-Definition INITIAL_WRITES_PUBDATA_HASHER_MIN_COST_IN_ERGS: u32 := int_mod_of 32 0%Z.
-Definition REPEATED_WRITES_PUBDATA_HASHER_MIN_COST_IN_ERGS: u32 := int_mod_of 32 0%Z.
+Definition VM_CYCLE_COST_IN_ERGS: ergs := int_mod_of 32 4%Z.
+Definition RAM_PERMUTATION_COST_IN_ERGS: ergs := int_mod_of 32 1%Z.
+Definition CODE_DECOMMITMENT_COST_PER_WORD_IN_ERGS: ergs := int_mod_of 32 4%Z.
+Definition STORAGE_APPLICATION_COST_IN_ERGS: ergs := int_mod_of 32 678%Z.
+Definition CODE_DECOMMITTER_SORTER_COST_IN_ERGS: ergs := int_mod_of 32 1%Z.
+Definition LOG_DEMUXER_COST_IN_ERGS: ergs := int_mod_of 32 1%Z.
+Definition STORAGE_SORTER_COST_IN_ERGS: ergs := int_mod_of 32 2%Z.
+Definition EVENTS_OR_L1_MESSAGES_SORTER_COST_IN_ERGS: ergs := int_mod_of 32 1%Z.
+Definition INITIAL_WRITES_PUBDATA_HASHER_COST_IN_ERGS: ergs := int_mod_of 32 18%Z.
+Definition REPEATED_WRITES_PUBDATA_HASHER_COST_IN_ERGS: ergs := int_mod_of 32 11%Z.
+Definition CODE_DECOMMITMENT_SORTER_COST_IN_ERGS: ergs := int_mod_of 32 1%Z.
 
-Definition STORAGE_WRITE_HASHER_MIN_COST_IN_ERGS: u32 := int_mod_of 32 0%Z.
+Definition L1_MESSAGE_MIN_COST_IN_ERGS: ergs := int_mod_of 32 156250%Z.
+Definition INITIAL_WRITES_PUBDATA_HASHER_MIN_COST_IN_ERGS: ergs := int_mod_of 32 0%Z.
+Definition REPEATED_WRITES_PUBDATA_HASHER_MIN_COST_IN_ERGS: ergs := int_mod_of 32 0%Z.
 
-Definition KECCAK256_CIRCUIT_COST_IN_ERGS: u32 := int_mod_of 32 40%Z.
-Definition SHA256_CIRCUIT_COST_IN_ERGS: u32 := int_mod_of 32 7%Z.
-Definition ECRECOVER_CIRCUIT_COST_IN_ERGS: u32 := int_mod_of 32 1112%Z.
+Definition STORAGE_WRITE_HASHER_MIN_COST_IN_ERGS: ergs := int_mod_of 32 0%Z.
 
-Definition INVALID_OPCODE_ERGS: u32 := ZMod.int_mod_of 32 (ZMod.unsigned_max 32).
+Definition KECCAK256_CIRCUIT_COST_IN_ERGS: ergs := int_mod_of 32 40%Z.
+Definition SHA256_CIRCUIT_COST_IN_ERGS: ergs := int_mod_of 32 7%Z.
+Definition ECRECOVER_CIRCUIT_COST_IN_ERGS: ergs := int_mod_of 32 1112%Z.
 
-Definition RICH_ADDRESSING_OPCODE_ERGS: u32
+Definition INVALID_OPCODE_ERGS: ergs := ZMod.int_mod_of 32 (ZMod.unsigned_max 32).
+
+Definition RICH_ADDRESSING_OPCODE_ERGS: ergs
   := int_mod_of 32 (VM_CYCLE_COST_IN_ERGS.(int_val _) + 2 * RAM_PERMUTATION_COST_IN_ERGS.(int_val _)).
-Definition AVERAGE_OPCODE_ERGS: u32
+Definition AVERAGE_OPCODE_ERGS: ergs
   := int_mod_of 32 (VM_CYCLE_COST_IN_ERGS.(int_val _)
                     + RAM_PERMUTATION_COST_IN_ERGS.(int_val _)).
 
 
 
-Definition STORAGE_READ_IO_PRICE: u32 := int_mod_of 32 150%Z.
-Definition STORAGE_WRITE_IO_PRICE: u32 :=int_mod_of 32  250%Z.
-Definition EVENT_IO_PRICE: u32 :=int_mod_of 32  25%Z.
-Definition L1_MESSAGE_IO_PRICE: u32 :=int_mod_of 32  100%Z.
-Definition CALL_LIKE_ERGS_COST: u32 :=int_mod_of 32  20%Z.
-Definition ERGS_PER_CODE_WORD_DECOMMITTMENT: u32 := CODE_DECOMMITMENT_COST_PER_WORD_IN_ERGS.
+Definition STORAGE_READ_IO_PRICE: ergs := int_mod_of 32 150%Z.
+Definition STORAGE_WRITE_IO_PRICE: ergs :=int_mod_of 32  250%Z.
+Definition EVENT_IO_PRICE: ergs :=int_mod_of 32  25%Z.
+Definition L1_MESSAGE_IO_PRICE: ergs :=int_mod_of 32  100%Z.
+Definition CALL_LIKE_ERGS_COST: ergs :=int_mod_of 32  20%Z.
+Definition ERGS_PER_CODE_WORD_DECOMMITTMENT: ergs := CODE_DECOMMITMENT_COST_PER_WORD_IN_ERGS.
+
+Definition growth_cost (diff: mem_address) : ergs := diff.
 
 End Ergs.
