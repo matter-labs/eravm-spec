9504d6fd7c8598330296d42c9087f4f7cac30c58
Igor Zhirkov <igorjirkov@gmail.com>
2023-05-19 18:00:35 +0200

    include comments in docs

--- build-docs.sh
+++ build-docs.sh
@@ -2,6 +2,6 @@ PYTHON=python3
 
 rm -rf doc
 mkdir -p doc
-coqdoc -toc --interpolate -utf8  -html -R src zkEVMSpec -d doc src/*.v && echo "Docs are generated in the 'doc' directory"
+coqdoc -toc --parse-comments --interpolate -utf8  -html -R src zkEVMSpec -d doc src/*.v && echo "Docs are generated in the 'doc' directory"
 $PYTHON process-docs.py dir doc && echo "Markdown in docs is processed"
 cp -f coqdoc.css doc
