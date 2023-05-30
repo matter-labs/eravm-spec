408856763e8f07fd4073c66e85a7b5b00fc65d8b
Igor Zhirkov <igorjirkov@gmail.com>
2023-05-30 16:04:41 +0200

    make coqdoc process all files recursively

--- build-docs.sh
+++ build-docs.sh
@@ -2,6 +2,6 @@ PYTHON=python3
 
 rm -rf doc
 mkdir -p doc
-coqdoc -toc --parse-comments --interpolate -utf8  -html -R src zkEVMSpec -d doc src/*.v && echo "Docs are generated in the 'doc' directory"
+coqdoc -toc --parse-comments --interpolate -utf8  -html -R src zkEVMSpec -d doc `find src -name '*.v'` && echo "Docs are generated in the 'doc' directory"
 $PYTHON process-docs.py dir doc && echo "Markdown in docs is processed"
 cp -f coqdoc.css doc
