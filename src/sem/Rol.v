5c2c5a41413b5182283201b0d30e50fd55c3841c
Igor Zhirkov <igorjirkov@gmail.com>
2023-06-25 23:32:07 +0300

    document and bugfix shifts

--- src/sem/Rol.v
+++ src/sem/Rol.v
@@ -5,12 +5,60 @@ Import Addressing ZArith Common Instruction Memory ZMod
 Section Def.
   Open Scope ZMod_scope.
   Inductive step: instruction -> smallstep :=
+    (**
+# Rol
+
+## Abstract Syntax
+
+```
+ | OpRol (in1: in_any) (in2: in_reg) (out: out_any) (swap:mod_swap) (flags:mod_set_flags)
+```
+
+## Syntax
+
+- `rol in1, in2, out`
+- `rol.s in1, in2, out`, to set `swap` modifier.
+- `rol! in1, in2, out`, to set `set flags` modifier.
+- `rol.s! in1, in2, out`, to set both `swap` and `set flags` modifiers.
+
+## Summary
+
+Bitwise circular left shift of `in1` by the number of binary digits specified by the lowest byte of `in2`. New binary digits (least significant bits in `out`) are taken from the most significant bits of `in1`.
+
+## Semantic
+
+Follows the scheme described in [binop_state_bitwise_effect_spec].
+
+- result is computed as `in1 <<< (in2 mod 256)`
+- flags are computed as follows:
+   - `EQ` is set if $result = 0$.
+   - other flags are reset
+
+Reminder: flags are only set if `set_flags` modifier is set. *)
   | step_Rol:
-    forall mod_swap mod_sf (in1:in_any) (in2:in_reg) out _tag1 _tag2 x y result gs gs',
+    forall mod_swap mod_sf (in1:in_any) (in2:in_reg) out _tag1 _tag2 x y result gs gs' shift,
       binop_state_bitwise_effect_spec in1 in2 out mod_swap mod_sf
         (mk_pv _tag1 x) (mk_pv _tag2 y) (IntValue result)  gs gs' ->
-      result = rol _ x y ->
+      shift = resize _ word_bits (resize _ 8 y) ->
+      result = rol _ x shift ->
       step (OpRol in1 in2 out mod_sf) gs gs'
   .
+(**
+## Affected parts of VM state
+
+- execution stack: PC, as by any instruction; SP, if `in1` uses `RelPop` addressing mode, or if `out` uses `RelPush` addressing mode.
+- Current stack memory page, if `out` resolves to it.
+- GPRs, if `out` resolves to a register.
+- flags, if `set_flags` modifier is set.
+
+## Usage
+
+- Operations with bit masks.
+- Fast arithmetic.
+
+## Similar instructions
+
+- `shl`, `shr`, `rol` and `ror` are encoded as variants of the same instruction.
+ *)
 
 End Def.
--- src/sem/Shl.v
+++ src/sem/Shl.v
@@ -6,59 +6,58 @@ Section Def.
   Open Scope ZMod_scope.
   Inductive step: instruction -> smallstep :=
   (**
+# Shl
 
-## Shl
-
-### Abstract Syntax
+## Abstract Syntax
 
 ```
- | OpShl (in1: in_any) (in2: in_reg) (out1: out_any) (swap:mod_swap) (flags:mod_set_flags)
+ | OpShl (in1: in_any) (in2: in_reg) (out: out_any) (swap:mod_swap) (flags:mod_set_flags)
 ```
 
-### Syntax
+## Syntax
 
 - `shl in1, in2, out`
 - `shl.s in1, in2, out`, to set `swap` modifier.
 - `shl! in1, in2, out`, to set `set flags` modifier.
 - `shl.s! in1, in2, out`, to set both `swap` and `set flags` modifiers.
 
-### Summary
+## Summary
 
-Bitwise left shift of `in1` by the number of binary digits specified by `in2`, but no more than 256. New binary digits (least significant bits) are zeros.
+Bitwise left shift of `in1` by the number of binary digits specified by the lowest byte of `in2`. New binary digits (least significant bits in `out`) are zeros.
 
-### Semantic
+## Semantic
 
 Follows the scheme described in [binop_state_bitwise_effect_spec].
 
-- result is computed as a bitwise XOR of two operands. 
+- result is computed as `in1 << (in2 mod 256)`
 - flags are computed as follows:
    - `EQ` is set if $result = 0$.
-   - `OF_LT` and `GT` are cleared
+   - other flags are reset
 
-Reminder: flags are only set if `set_flags` modifier is set.
-
-### Affected parts of VM state
+Reminder: flags are only set if `set_flags` modifier is set. *)
+  | step_Shl:
+    forall mod_swap mod_sf (in1:in_any) (in2:in_reg) out _tag1 _tag2 x y result gs gs' shift,
+      binop_state_bitwise_effect_spec in1 in2 out mod_swap mod_sf
+        (mk_pv _tag1 x) (mk_pv _tag2 y) (IntValue result)  gs gs' ->
+      shift = resize _ word_bits (resize _ 8 y) ->
+      result = shiftl _ x shift ->
+      step (OpShl in1 in2 out mod_sf) gs gs'
+  .
+(**
+## Affected parts of VM state
 
 - execution stack: PC, as by any instruction; SP, if `in1` uses `RelPop` addressing mode, or if `out` uses `RelPush` addressing mode.
 - Current stack memory page, if `out` resolves to it.
 - GPRs, if `out` resolves to a register.
 - flags, if `set_flags` modifier is set.
 
-### Usage
+## Usage
 
-- operations with bit masks
+- Operations with bit masks.
+- Fast arithmetic.
 
-### Similar instructions
+## Similar instructions
 
-- `and`, `or` and `xor` are encoded as variants of the same instruction.
-
-   *)
-  | step_Shl:
-    forall mod_swap mod_sf (in1:in_any) (in2:in_reg) out _tag1 _tag2 x y result gs gs',
-      binop_state_bitwise_effect_spec in1 in2 out mod_swap mod_sf
-        (mk_pv _tag1 x) (mk_pv _tag2 y) (IntValue result)  gs gs' ->
-      result = shiftl _ x y ->
-      step (OpShl in1 in2 out mod_sf) gs gs'
-  .
-
+- `shl`, `shr`, `rol` and `ror` are encoded as variants of the same instruction.
+ *)
 End Def.
