416ee92db68d8e09da1c362ae844ebc89b759d10
Igor Zhirkov <igorjirkov@gmail.com>
2023-06-26 11:24:02 +0300

    document store and store inc

--- src/sem/Store.v
+++ src/sem/Store.v
@@ -19,6 +19,50 @@ Section Defs.
   Let stores := resolve_stores old_xstack (old_regs,old_pages).
   
   Inductive step_store: instruction -> regs_state * callstack * memory -> Prop :=
+(**
+# Store
+
+## Abstract Syntax
+
+```
+OpStore (ptr: in_regimm) (val: in_reg) (mem:data_page_type) 
+```
+
+## Syntax
+
+- `uma.heap_write in1, in2` aliased as `st.1 in1, out`
+- `uma.aux_heap_write in1, in2` aliased as `st.2 in1, out`
+
+
+## Summary
+
+Store 32 consecutive bytes to the active `heap` or `aux_heap` page starting from
+address `in1`.
+
+## Semantic
+
+1. Decode a fat pointer `in_ptr` from `ptr`.
+
+   Fat pointers have following fields:
+
+```
+Record fat_ptr :=
+  mk_fat_ptr {
+      fp_page: page_id;
+      fp_start: mem_address;
+      fp_length: mem_address;
+      fp_offset: mem_address;
+    }.
+```
+
+2. Ensure storing 32 consecutive bytes is possible; it is impossible if $\texttt{fp\_offset} > 2^{32}-32$.
+
+   Note: Valid (aux_)heap fat pointers always have `fp_start = 0`, therefore the read starts from an absolute address `fp_offset` in heap/auxheap. See [fat_ptr].
+
+3. If  `fp_offset + 32 > (aux_)heap bound`, grow (aux_)heap bound and pay for the growth. We are aiming at reading a 256-bit word starting from address `fp_offset`, so the (aux_)heap bound should contain all of it.
+4. Store 32 consecutive bytes of `val` as a Big Endian 256-bit word from address `fp_offset` in (aux_)heap.
+*)
+    
   | step_Store:
     forall new_xstack heap_variant enc_ptr (arg_enc_ptr:in_regimm) (arg_val:in_reg) value new_regs new_pages selected_page in_ptr query modified_page,
 
@@ -44,5 +88,26 @@ Section Defs.
 
       step_store (OpStore arg_enc_ptr arg_val heap_variant) (new_regs, new_xstack, new_pages)
   .
+(**
+## Affected parts of VM state
 
+- execution stack:
+
+  + PC, as by any instruction;
+  + ergs balance if the (aux_)heap has to be grown;
+  + (aux_)heap bounds, if (aux_)heap has to be grown.
+
+- GPRs, because `out` only resolves to a register.
+- Memory page
+
+## Usage
+
+- Only [OpLoad] and [OpLoadInc] are capable of reading data from heap/aux_heap.
+- One of few instructions that accept only reg or imm operand but do not have full addressing mode, therefore can't e.g. address stack. The full list is: [OpLoad], [OpLoadInc], [OpStore], [OpStoreInc], [OpLoadPointer], [OpLoadPointerInc].
+
+## Similar instructions
+
+- [OpLoad], [OpLoadInc], [OpStore], [OpStoreInc], [OpLoadPointer], [OpLoadPointerInc] are variants of the same instruction.
+
+ *)
 End Defs. 
--- src/sem/StoreInc.v
+++ src/sem/StoreInc.v
@@ -19,7 +19,57 @@ Section Defs.
   Let stores := resolve_stores old_xstack (old_regs,old_pages).
   
   Inductive step_store: instruction -> regs_state * callstack * memory -> Prop :=
+(**
+# StoreInc
 
+## Abstract Syntax
+
+```
+OpStoreInc (ptr: in_regimm) (val: in_reg) (mem:data_page_type) (inc_ptr: out_reg)
+```
+
+## Syntax
+
+- `uma.inc.heap_write in1, in2` aliased as `st.1.inc in1, out`
+- `uma.inc.aux_heap_write in1, in2` aliased as `st.2.inc in1, out`
+
+
+## Summary
+
+Store 32 consecutive bytes to the active `heap` or `aux_heap` page starting from
+address `ptr`. Additionally, store a pointer to the next word to `art_ptr` register.
+
+## Semantic
+
+1. Decode a fat pointer `in_ptr` from `ptr`.
+
+   Fat pointers have following fields:
+
+```
+Record fat_ptr :=
+  mk_fat_ptr {
+      fp_page: page_id;
+      fp_start: mem_address;
+      fp_length: mem_address;
+      fp_offset: mem_address;
+    }.
+```
+
+2. Ensure storing 32 consecutive bytes is possible; it is impossible if $\texttt{fp\_offset} > 2^{32}-32$.
+
+   Note: Valid (aux_)heap fat pointers always have `fp_start = 0`, therefore the read starts from an absolute address `fp_offset` in heap/auxheap. See [fat_ptr].
+
+3. If  `fp_offset + 32 > (aux_)heap bound`, grow (aux_)heap bound and pay for the growth. We are aiming at reading a 256-bit word starting from address `fp_offset`, so the (aux_)heap bound should contain all of it.
+4. Store 32 consecutive bytes of `val` as a Big Endian 256-bit word from address `fp_offset` in (aux_)heap.
+5. Store an encoded fat pointer to the next 32-byte word in (aux_)heap in `inc_ptr`. Its fields are assigned as follows:
+
+```
+fp_page := (aux_)heap page id;
+fp_start := zero32;
+fp_length := in_ptr.(fp_length);
+fp_offset := in_ptr.(fp_offset) + 32;
+```   
+*)
   | step_StoreInc:
     forall new_xstack heap_variant enc_ptr (arg_modptr:out_reg) (arg_enc_ptr:in_regimm) (arg_val:in_reg) value new_regs new_pages selected_page in_ptr ptr_incremented pages1 query modified_page,
 
@@ -56,5 +106,25 @@ Section Defs.
       step_store (OpStoreInc arg_enc_ptr arg_val heap_variant arg_modptr)
         (new_regs, new_xstack, new_pages)
   .
+(**
+## Affected parts of VM state
 
+- execution stack:
+
+  + PC, as by any instruction;
+  + ergs balance if the (aux_)heap has to be grown;
+  + (aux_)heap bounds, if (aux_)heap has to be grown.
+
+- GPRs, because `res` and `inc_ptr` only resolve to registers.
+
+## Usage
+
+- Only [OpStore] and [OpStoreInc] are capable of writing to (aux_)heap.
+- One of few instructions that accept only reg or imm operand but do not have full addressing mode, therefore can't e.g. address stack. The full list is: [OpLoad], [OpLoadInc], [OpStore], [OpStoreInc], [OpLoadPointer], [OpLoadPointerInc].
+
+## Similar instructions
+
+- [OpLoad], [OpLoadInc], [OpStore], [OpStoreInc], [OpLoadPointer], [OpLoadPointerInc] are variants of the same instruction.
+
+ *)
 End Defs. 
