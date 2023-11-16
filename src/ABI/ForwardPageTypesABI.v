e1ecab15ad508a578e3fce90e2b9e1a52acab87b
Igor Zhirkov <iz@matterlabs.dev>
2023-11-16 14:25:04 +0100

    add missing check for offset=0 in fatptr layout when decoding ForwardNewFatPointer

--- src/ABI/ForwardPageTypesABI.v
+++ src/ABI/ForwardPageTypesABI.v
@@ -16,19 +16,25 @@ Definition data_page_type_to_u8 (t:data_page_type) : u8 :=
   | AuxHeap =>FarCallForwardPageType.UseAuxHeap
   end.
 
-Definition span_of (fp: fat_ptr_layout) : span :=
-  mk_span fp.(start) fp.(length).
+Definition span_of (fp: fat_ptr_layout) : option span :=
+  if fp.(offset) == zero32
+  then None
+  else Some (mk_span fp.(start) fp.(length))
+.
 
 Definition fwd_memory_adapter (fwd_type: u8) (raw_fat_ptr_layout: fat_ptr_layout) : option MemoryManagement.fwd_memory:=
   if fwd_type == FarCallForwardPageType.ForwardFatPointer then
     option_map ForwardExistingFatPointer
       (FatPointerABI.decode_layout raw_fat_ptr_layout)
   else
-    let span := span_of raw_fat_ptr_layout in
-    Some (
-        if fwd_type == FarCallForwardPageType.UseAuxHeap then
-          ForwardNewFatPointer AuxHeap span
-        else (* Heap or a default option *)
-          ForwardNewFatPointer Heap span
-      )
+    match span_of raw_fat_ptr_layout with
+    | Some span =>
+        Some (
+            if fwd_type == FarCallForwardPageType.UseAuxHeap then
+              ForwardNewFatPointer AuxHeap span
+            else (* Heap or a default option *)
+              ForwardNewFatPointer Heap span
+          )
+    | None => None
+    end
 .
